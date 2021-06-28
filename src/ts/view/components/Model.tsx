71988d1cdbe339e7e8552dd3d0fc3d1fc40250e4
ryotarohada <hada@monster-dive.com>
2021-06-28 15:50:37 +0900

    Deleate Test

--- src/ts/view/components/Model.tsx
+++ src/ts/view/components/Model.tsx
@@ -1,18 +1,13 @@
 import * as React from "react";
-import * as Fiber from '@react-three/fiber';
 import * as Drei from "@react-three/drei";
 
-interface Props {
-    position?: [number, number,number]
-};
-
-const Model: React.FC<Props> = (props) => {
+const Model: React.FC = () => {
     
-    const { scene } = Drei.useGLTF("assets/gltf/model.glb");
+    const { scene } = Drei.useGLTF("assets/gltf/rocket.glb");
 
   return (
-      <group {...props} dispose={null}>
-        <primitive scale={[1, 1, 1]} object={scene} />
+      <group>
+        <primitive object={scene} />
       </group>
     )
 };
